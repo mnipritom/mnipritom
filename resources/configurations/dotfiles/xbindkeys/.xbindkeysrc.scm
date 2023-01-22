021778562fd5b7f2551ea7b6ddb293e890b23467
那思路(なしろ) <5481667-mnipritom@users.noreply.gitlab.com>
2023-01-23 02:39:33 +0600

    added `xbindkeys` dotfiles

---
+++ resources/configurations/dotfiles/xbindkeys/.xbindkeysrc.scm
@@ -0,0 +1,19 @@
+;;(getenv "TERMINAL")
+;;(getenv "EDITOR")
+
+;;(system " ")
+
+(xbindkey '(Mod4 r) "rofi -show")
+(xbindkey '(Mod4 t) "rofi -show run")
+(xbindkey '(Mod4 w) "xdo close")
+(xbindkey '(Mod4 q) "xdo kill")
+
+;;(xbindkey '(XF86AudioPrev) " ")
+;;(xbindkey '(XF86AudioNext) " ")
+;;(xbindkey '(XF86AudioPlay) " ")
+;;(xbindkey '(XF86AudioStop) " ")
+
+;;(xbindkeys '(XF86AudioRaiseVolume) " ")
+;;(xbindkeys '(XF86AudioLowerVolume) " ")
+;;(xbindkeys '(XF86AudioMute) " ")
+;;(xbindkeys '(XF86AudioMicMute) " ")
