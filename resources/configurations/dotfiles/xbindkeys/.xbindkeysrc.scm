76a51116fc14bed2700639fcd2c4102304e0d402
那思路(なしろ) <10971185+mnipritom@users.noreply.github.com>
2023-01-23 23:18:01 +0600

    updated `xbindkeys` configuration

--- resources/configurations/dotfiles/xbindkeys/.xbindkeysrc.scm
+++ resources/configurations/dotfiles/xbindkeys/.xbindkeysrc.scm
@@ -1,19 +1,14 @@
-;;(getenv "TERMINAL")
-;;(getenv "EDITOR")
-
-;;(system " ")
-
 (xbindkey '(Mod4 r) "rofi -show")
 (xbindkey '(Mod4 t) "rofi -show run")
 (xbindkey '(Mod4 w) "xdo close")
 (xbindkey '(Mod4 q) "xdo kill")
 
-;;(xbindkey '(XF86AudioPrev) " ")
-;;(xbindkey '(XF86AudioNext) " ")
-;;(xbindkey '(XF86AudioPlay) " ")
-;;(xbindkey '(XF86AudioStop) " ")
+;(xbindkey '(XF86AudioPrev) " ")
+;(xbindkey '(XF86AudioNext) " ")
+;(xbindkey '(XF86AudioPlay) " ")
+;(xbindkey '(XF86AudioStop) " ")
 
-;;(xbindkeys '(XF86AudioRaiseVolume) " ")
-;;(xbindkeys '(XF86AudioLowerVolume) " ")
-;;(xbindkeys '(XF86AudioMute) " ")
-;;(xbindkeys '(XF86AudioMicMute) " ")
+;(xbindkey '(XF86AudioRaiseVolume) " ")
+;(xbindkey '(XF86AudioLowerVolume) " ")
+;(xbindkey '(XF86AudioMute) " ")
+;(xbindkey '(XF86AudioMicMute) " ")
