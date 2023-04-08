ab9674e9a9dcfa16c206af79175d35450329f753
那思路(なしろ) <10971185+mnipritom@users.noreply.github.com>
2023-04-08 14:20:09 +0600

    made minor changes
    Co-Authored-By: 那思路(なしろ) <5481667-mnipritom@users.noreply.gitlab.com>

--- sources/lua/frameworks/neovim/references/plugins/resources/plenary.lua
+++ sources/lua/frameworks/neovim/references/plugins/resources/plenary.lua
@@ -1,9 +1,3 @@
 return {
-  dir = neovimSourcesPath .. "references/plugins/sources/plenary",
-  config = function()
-    -- [LINK] https://github.com/nvim-lua/plenary.nvim#getting-started
-    local plenary = require("plenary")
-    local async = require("plenary.async")
-    return plenary, async
-  end
+  dir = neovimSourcesPath .. "references/plugins/sources/plenary"
 }
