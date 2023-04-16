25adc299c39585bc5f57fe619f62afa1d93d0176
那思路(なしろ) <10971185+mnipritom@users.noreply.github.com>
2023-04-17 04:27:57 +0600

    did better 😤
    Co-Authored-By: 那思路(なしろ) <5481667-mnipritom@users.noreply.gitlab.com>

--- sources/lua/frameworks/xplr/init.lua
+++ sources/lua/frameworks/xplr/init.lua
@@ -1,5 +1,7 @@
+version = "0.20.1"
+
 -- [NOTE] added extra `/` to denote root of filesystem
--- [NOTE] realpath sometimes strips off preceding `/`
+-- [NOTE] `io` output of realpath sometimes strips off preceding `/`
 local xplrSourcesEntryPointPath = debug.getinfo(1,"S").source:sub(2)
 xplrSourcesEntryPointPath = io.popen("realpath '" .. "/" .. xplrSourcesEntryPointPath .. "'", "r"):read("a")
 xplrSourcesEntryPointPath = xplrSourcesEntryPointPath:gsub("[\n\r]*$","")
@@ -10,8 +12,6 @@ xplrEntryPointFileName = xplrEntryPointFileName or xplrSourcesEntryPointPath
 
 package.path = xplrSourcesDirectoryPath .. "?.lua;" .. package.path
 package.path = xplrSourcesDirectoryPath .. "?/init.lua;" .. package.path
-
-version = "0.20.1"
 
 productions = "productions"
 
--- sources/lua/frameworks/xplr/productions/layouts.lua
+++ sources/lua/frameworks/xplr/productions/layouts.lua
@@ -1,21 +1,21 @@
-local minimalLayout = {
-  Vertical = {
-    splits = {
-      "Table"
-    },
-    config = {
-      margin = nil,
-      horizontal_margin = 0,
-      vertical_margin = 0,
-      constraints = {
-        { 
-          Percentage = 100
+xplr.config.layouts.custom = {
+  hierarchy = {
+    Vertical = {
+      splits = {
+        "Table"
+      },
+      config = {
+        margin = nil,
+        horizontal_margin = nil,
+        vertical_margin = nil,
+        constraints = {
+          { 
+            Percentage = 100
+          }
         }
       }
     }
   }
 }
 
-xplr.config.layouts.custom.minimalLayout = minimalLayout
-
-xplr.config.general.initial_layout = "minimalLayout"
+xplr.config.general.initial_layout = "hierarchy"
